0f8fd5662d190efc1eeba1bc1bd1163f680dc389
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-01-20 20:31:43 +0200

    Setting up HTML handler

--- handlers/htmlHandler.js
+++ handlers/htmlHandler.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
+const getHtmlContent = (filename) => {
+    const filePath = path.join(__dirname, '../assets/Pages/', filename);
+    const content = require('fs').readFileSync(filePath, 'utf-8');
+    return content;
+};
+
 router.get('/html1', (req, res) => {
-    res.send('This is HTML1');
+    const htmlContent = getHtmlContent('Page1.html');
+    res.send(htmlContent);
 });
 
 router.get('/html2', (req, res) => {
-    res.send('This is HTML2');
+    const htmlContent = getHtmlContent('Page2.html');
+    res.send(htmlContent);
 });
 
 module.exports = router;
